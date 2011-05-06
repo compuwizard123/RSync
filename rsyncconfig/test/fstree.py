8fec3d528abdd01a78b62cb3b088887da14c7e8a
Thomas W. Most <twm@freecog.net>
2011-05-06 01:37:03 -0400

    FSTree tests: Add comments and mock stat.S_ISDIR

--- rsyncconfig/test/fstree.py
+++ rsyncconfig/test/fstree.py
@@ -17,11 +17,32 @@
 Tests for the rsyncconfig.fstree module
 '''
 
+import stat
 import unittest
+from contextlib import contextmanager
 
-from mock import sentinel
+from mock import patch, sentinel
 
 from rsyncconfig.fstree import FSTree
+
+
+@contextmanager
+def mocked_stat():
+    '''Context manager which mocks stat.S_ISDIR.
+    '''
+    def S_ISDIR(stat_t):
+        if stat_t is sentinel.file_stat:
+            return False
+        if stat_t is sentinel.dir_stat:
+            return True
+        raise Exception("mocked_stat doesn't support {0!r}".format(stat_t))
+
+    original_S_ISDIR = stat.S_ISDIR
+    stat.S_ISDIR = S_ISDIR
+    try:
+        yield
+    finally:
+        stat.S_ISDIR = original_S_ISDIR
 
 
 def store_to_list(tree_store):
@@ -40,32 +61,50 @@ class TestFSTree(unittest.TestCase):
         self.fstree = FSTree()
 
     def test_add_single_file(self):
-        self.fstree.add_path('foo', sentinel.file_stat, False)
-        self.assertEqual([('foo', sentinel.file_stat, False, [])],
-                         store_to_list(self.fstree.store))
+        '''Check that the FSTree updates correctly when a single file is added.
+        '''
+        with mocked_stat():
+            self.fstree.add_path('foo', sentinel.file_stat, False)
+            self.assertEqual([('foo', sentinel.file_stat, False, [])],
+                             store_to_list(self.fstree.store))
 
     def test_add_two_files(self):
-        self.fstree.add_path('foo', sentinel.foo_stat, False)
-        self.fstree.add_path('bar', sentinel.bar_stat, False)
-        self.assertEqual([('bar', sentinel.bar_stat, False, []),
-                          ('foo', sentinel.foo_stat, False, [])],
-                         store_to_list(self.fstree.store))
+        '''Check that the FSTree updates correctly when two files are added.
+
+        The FSTree should sort the files lexicographically by name.
+        '''
+        with mocked_stat():
+            self.fstree.add_path('foo', sentinel.file_stat, False)
+            self.fstree.add_path('bar', sentinel.file_stat, False)
+            self.assertEqual([('bar', sentinel.file_stat, False, []),
+                              ('foo', sentinel.file_stat, False, [])],
+                             store_to_list(self.fstree.store))
 
     def test_add_dir_with_file(self):
-        self.fstree.add_path('dir', sentinel.dir_stat)
-        self.fstree.add_path('dir/file', sentinel.file_stat, False)
-        self.assertEqual([('dir', sentinel.dir_stat, True, [
-            ('dir/file', sentinel.file_stat, False, []),
-        ])], store_to_list(self.fstree.store))
+        '''Check that FSTree updates correctly with a dir containing a file.
+
+        The file should be placed inside the directory.
+        '''
+        with mocked_stat():
+            self.fstree.add_path('dir', sentinel.dir_stat)
+            self.fstree.add_path('dir/file', sentinel.file_stat, False)
+            self.assertEqual([('dir', sentinel.dir_stat, True, [
+                ('dir/file', sentinel.file_stat, False, []),
+            ])], store_to_list(self.fstree.store))
 
     def test_add_dir_with_files(self):
-        self.fstree.add_path('dir', sentinel.dir_stat)
-        self.fstree.add_path('dir/foo', sentinel.foo_stat, False)
-        self.fstree.add_path('dir/bar', sentinel.bar_stat, False)
-        self.assertEqual([('bar', sentinel.dir_stat, True, [
-            ('bar', sentinel.bar_stat, False, []),
-            ('foo', sentinel.foo_stat, False, [])
-        ])], store_to_list(self.fstree.store))
+        '''Check that two files within a directory are handled properly.
+
+        The files should be sorted lexicographically by name.
+        '''
+        with mocked_stat():
+            self.fstree.add_path('dir', sentinel.dir_stat)
+            self.fstree.add_path('dir/foo', sentinel.file_stat, False)
+            self.fstree.add_path('dir/bar', sentinel.file_stat, False)
+            self.assertEqual([('bar', sentinel.dir_stat, True, [
+                ('dir/bar', sentinel.bar_stat, False, []),
+                ('dir/foo', sentinel.foo_stat, False, [])
+            ])], store_to_list(self.fstree.store))
 
 
 def get_suite():
